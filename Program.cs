a48e3f0350bd066117dfc404d2dd66ad2ed10038
Lester-23 <79073749+Lester-23@users.noreply.github.com>
2021-02-20 00:30:42 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AñoBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Ingrese el año que desea consultar: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a%4 != 0)
+            {
+                Console.WriteLine("\nEl año no es bisiesto");
+            }
+            else if(a%100 != 0)
+            {
+                Console.WriteLine("\nEl año es bisiesto");
+            }
+
+        }      
+
+    }
+}
+
